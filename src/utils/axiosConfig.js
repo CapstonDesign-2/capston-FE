70e92cb593426617bd8b272cc4a9aa37ee1d54ad
seungyun-Park <tmdrbs0925@gmail.com>
2024-11-19 01:54:25 +0900

    header-추가

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -3,8 +3,10 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:3000',
   headers: {
+    "Cache-Control": "no-cache",
+    'Accept': 'application/json',
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
+    "Access-Control-Allow-Origin": "*",
   },
 });
 
