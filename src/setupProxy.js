32c3b6cac607aeb1f80dec182cdbde4828523789
seungyun-Park <tmdrbs0925@gmail.com>
2024-11-20 11:38:12 +0900

    3000포트 프록시 임시 비활성

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,13 +1,13 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (app) => {
-  app.use(
-    "/api/system-info",
-    createProxyMiddleware({
-      target: "http://localhost:3000",
-      changeOrigin: true,
-    })
-  );
+  // app.use(
+  //   "/api/system-info",
+  //   createProxyMiddleware({
+  //     target: "http://localhost:3000",
+  //     changeOrigin: true,
+  //   })
+  // );
 
   app.use(
     "/api",
